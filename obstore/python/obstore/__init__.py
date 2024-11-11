23f715bd47742dc96772059970631347ac1da448
Kyle Barron <kyle@developmentseed.org>
2024-11-11 15:15:22 -0500

    Remove runtime circular import (#84)

--- obstore/python/obstore/__init__.py
+++ obstore/python/obstore/__init__.py
@@ -1,5 +1,9 @@
-from . import store
+from typing import TYPE_CHECKING
+
 from ._obstore import *
 from ._obstore import ___version
+
+if TYPE_CHECKING:
+    from . import store
 
 __version__: str = ___version()
