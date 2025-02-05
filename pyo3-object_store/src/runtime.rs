71e873a395ae023dc9c289f8828c94f4f4d09fc7
Kyle Barron <kyle@developmentseed.org>
2025-02-04 19:14:20 -0500

    Check for forked process when accessing tokio runtime (#214)
    * Check for a forked process when accessing tokio runtime
    
    * Remove unnecessary Arc

--- pyo3-object_store/src/runtime.rs
+++ pyo3-object_store/src/runtime.rs
@@ -1,11 +1,10 @@
-use std::sync::Arc;
-
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pyo3::sync::GILOnceCell;
 use tokio::runtime::Runtime;
 
-static RUNTIME: GILOnceCell<Arc<Runtime>> = GILOnceCell::new();
+static RUNTIME: GILOnceCell<Runtime> = GILOnceCell::new();
+static PID: GILOnceCell<u32> = GILOnceCell::new();
 
 /// Construct a tokio runtime for sync requests
 ///
@@ -17,11 +16,23 @@ static RUNTIME: GILOnceCell<Arc<Runtime>> = GILOnceCell::new();
 ///
 /// Downstream consumers may explicitly want to depend on tokio and add `rt-multi-thread` as a
 /// tokio feature flag to opt-in to the multi-threaded tokio runtime.
-pub fn get_runtime(py: Python<'_>) -> PyResult<Arc<Runtime>> {
+pub fn get_runtime(py: Python<'_>) -> PyResult<&'static Runtime> {
+    let pid = std::process::id();
+    let runtime_pid = *PID.get_or_init(py, || pid);
+    if pid != runtime_pid {
+        panic!(
+            "Forked process detected - current PID is {} but the tokio runtime was created by {}. The tokio \
+            runtime does not support forked processes https://github.com/tokio-rs/tokio/issues/4301. If you are \
+            seeing this message while using Python multithreading make sure to use the `spawn` or `forkserver` \
+            mode.",
+            pid, runtime_pid
+        );
+    }
+
     let runtime = RUNTIME.get_or_try_init(py, || {
-        Ok::<_, PyErr>(Arc::new(Runtime::new().map_err(|err| {
+        Ok::<_, PyErr>(Runtime::new().map_err(|err| {
             PyValueError::new_err(format!("Could not create tokio runtime. {}", err))
-        })?))
+        })?)
     })?;
-    Ok(runtime.clone())
+    Ok(runtime)
 }
