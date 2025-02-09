f6ba27cb1a97625bb457e14fe560b5d2a1eeb9d8
machichima <nary12321@gmail.com>
2025-02-09 11:21:04 +0800

    test: update test with new path format
    path with bucket name

--- tests/test_fsspec.py
+++ tests/test_fsspec.py
@@ -4,7 +4,6 @@ import fsspec
 import pyarrow.parquet as pq
 import pytest
 
-import obstore as obs
 from obstore.fsspec import AsyncFsspecStore, register
 from tests.conftest import TEST_BUCKET_NAME
 
@@ -30,9 +29,7 @@ def test_register():
     # test register asynchronous
     register("gcs", asynchronous=True)  # Register the "s3" protocol dynamically
     fs_class = fsspec.get_filesystem_class("gcs")
-    assert (
-        fs_class.asynchronous == True
-    ), "Registered class should be asynchronous"
+    assert fs_class.asynchronous == True, "Registered class should be asynchronous"
 
     # test multiple registrations
     register(["file", "abfs"])
@@ -57,6 +54,7 @@ def test_register_invalid_types():
     with pytest.raises(ValueError):
         register([])  # Empty list is invalid
 
+
 @pytest.fixture()
 def fs(s3_store_config):
     register("s3")
@@ -64,88 +62,113 @@ def fs(s3_store_config):
 
 
 def test_list(fs):
-    out = fs.ls("", detail=False)
-    assert out == ["afile"]
-    fs.pipe_file("dir/bfile", b"data")
-    out = fs.ls("", detail=False)
-    assert out == ["afile", "dir"]
-    out = fs.ls("", detail=True)
+    out = fs.ls(f"{TEST_BUCKET_NAME}", detail=False)
+    assert out == [f"{TEST_BUCKET_NAME}/afile"]
+    fs.pipe_file(f"{TEST_BUCKET_NAME}/dir/bfile", b"data")
+    out = fs.ls(f"{TEST_BUCKET_NAME}", detail=False)
+    assert out == [f"{TEST_BUCKET_NAME}/afile", f"{TEST_BUCKET_NAME}/dir"]
+    out = fs.ls(f"{TEST_BUCKET_NAME}", detail=True)
     assert out[0]["type"] == "file"
     assert out[1]["type"] == "directory"
 
 
 @pytest.mark.asyncio
-async def test_list_async(s3_store):
-    fs = AsyncFsspecStore(s3_store, asynchronous=True)
-    out = await fs._ls("", detail=False)
-    assert out == ["afile"]
-    await fs._pipe_file("dir/bfile", b"data")
-    out = await fs._ls("", detail=False)
-    assert out == ["afile", "dir"]
-    out = await fs._ls("", detail=True)
+async def test_list_async(s3_store_config):
+    register("s3")
+    fs = fsspec.filesystem("s3", config=s3_store_config, asynchronous=True)
+
+    out = await fs._ls(f"{TEST_BUCKET_NAME}", detail=False)
+    assert out == [f"{TEST_BUCKET_NAME}/afile"]
+    await fs._pipe_file(f"{TEST_BUCKET_NAME}/dir/bfile", b"data")
+    out = await fs._ls(f"{TEST_BUCKET_NAME}", detail=False)
+    assert out == [f"{TEST_BUCKET_NAME}/afile", f"{TEST_BUCKET_NAME}/dir"]
+    out = await fs._ls(f"{TEST_BUCKET_NAME}", detail=True)
     assert out[0]["type"] == "file"
     assert out[1]["type"] == "directory"
 
 
 @pytest.mark.network
 def test_remote_parquet():
-    store = obs.store.HTTPStore.from_url("https://github.com")
-    fs = AsyncFsspecStore(store)
-    url = "opengeospatial/geoparquet/raw/refs/heads/main/examples/example.parquet"
+    register("https")
+    fs = fsspec.filesystem("https")
+    url = "github.com/opengeospatial/geoparquet/raw/refs/heads/main/examples/example.parquet"
+    pq.read_metadata(url, filesystem=fs)
+
+    # also test with full url
+    url = "https://github.com/opengeospatial/geoparquet/raw/refs/heads/main/examples/example.parquet"
     pq.read_metadata(url, filesystem=fs)
 
 
 def test_multi_file_ops(fs):
-    data = {"dir/test1": b"test data1", "dir/test2": b"test data2"}
+    data = {
+        f"{TEST_BUCKET_NAME}/dir/test1": b"test data1",
+        f"{TEST_BUCKET_NAME}/dir/test2": b"test data2",
+    }
     fs.pipe(data)
     out = fs.cat(list(data))
     assert out == data
-    out = fs.cat("dir", recursive=True)
+    out = fs.cat(f"{TEST_BUCKET_NAME}/dir", recursive=True)
     assert out == data
-    fs.cp("dir", "dir2", recursive=True)
-    out = fs.find("", detail=False)
-    assert out == ["afile", "dir/test1", "dir/test2", "dir2/test1", "dir2/test2"]
-    fs.rm(["dir", "dir2"], recursive=True)
-    out = fs.find("", detail=False)
-    assert out == ["afile"]
+    fs.cp(f"{TEST_BUCKET_NAME}/dir", f"{TEST_BUCKET_NAME}/dir2", recursive=True)
+    out = fs.find(f"{TEST_BUCKET_NAME}", detail=False)
+    assert out == [
+        f"{TEST_BUCKET_NAME}/afile",
+        f"{TEST_BUCKET_NAME}/dir/test1",
+        f"{TEST_BUCKET_NAME}/dir/test2",
+        f"{TEST_BUCKET_NAME}/dir2/test1",
+        f"{TEST_BUCKET_NAME}/dir2/test2",
+    ]
+    fs.rm([f"{TEST_BUCKET_NAME}/dir", f"{TEST_BUCKET_NAME}/dir2"], recursive=True)
+    out = fs.find(f"{TEST_BUCKET_NAME}", detail=False)
+    assert out == [f"{TEST_BUCKET_NAME}/afile"]
 
 
 def test_cat_ranges_one(fs):
     data1 = os.urandom(10000)
-    fs.pipe_file("data1", data1)
+    fs.pipe_file(f"{TEST_BUCKET_NAME}/data1", data1)
 
     # single range
-    out = fs.cat_ranges(["data1"], [10], [20])
+    out = fs.cat_ranges([f"{TEST_BUCKET_NAME}/data1"], [10], [20])
     assert out == [data1[10:20]]
 
     # range oob
-    out = fs.cat_ranges(["data1"], [0], [11000])
+    out = fs.cat_ranges([f"{TEST_BUCKET_NAME}/data1"], [0], [11000])
     assert out == [data1]
 
     # two disjoint ranges, one file
-    out = fs.cat_ranges(["data1", "data1"], [10, 40], [20, 60])
+    out = fs.cat_ranges(
+        [f"{TEST_BUCKET_NAME}/data1", f"{TEST_BUCKET_NAME}/data1"], [10, 40], [20, 60]
+    )
     assert out == [data1[10:20], data1[40:60]]
 
     # two adjoining ranges, one file
-    out = fs.cat_ranges(["data1", "data1"], [10, 30], [20, 60])
+    out = fs.cat_ranges(
+        [f"{TEST_BUCKET_NAME}/data1", f"{TEST_BUCKET_NAME}/data1"], [10, 30], [20, 60]
+    )
     assert out == [data1[10:20], data1[30:60]]
 
     # two overlapping ranges, one file
-    out = fs.cat_ranges(["data1", "data1"], [10, 15], [20, 60])
+    out = fs.cat_ranges(
+        [f"{TEST_BUCKET_NAME}/data1", f"{TEST_BUCKET_NAME}/data1"], [10, 15], [20, 60]
+    )
     assert out == [data1[10:20], data1[15:60]]
 
     # completely overlapping ranges, one file
-    out = fs.cat_ranges(["data1", "data1"], [10, 0], [20, 60])
+    out = fs.cat_ranges(
+        [f"{TEST_BUCKET_NAME}/data1", f"{TEST_BUCKET_NAME}/data1"], [10, 0], [20, 60]
+    )
     assert out == [data1[10:20], data1[0:60]]
 
 
 def test_cat_ranges_two(fs):
     data1 = os.urandom(10000)
     data2 = os.urandom(10000)
-    fs.pipe({"data1": data1, "data2": data2})
+    fs.pipe({f"{TEST_BUCKET_NAME}/data1": data1, f"{TEST_BUCKET_NAME}/data2": data2})
 
     # single range in each file
-    out = fs.cat_ranges(["data1", "data2"], [10, 10], [20, 20])
+    out = fs.cat_ranges(
+        [f"{TEST_BUCKET_NAME}/data1", f"{TEST_BUCKET_NAME}/data2"], [10, 10], [20, 20]
+    )
     assert out == [data1[10:20], data2[10:20]]
 
 
@@ -170,4 +193,4 @@ def test_atomic_write(fs):
 
 def test_cat_ranges_error(fs):
     with pytest.raises(ValueError):
-        fs.cat_ranges(["path"], [], [])
+        fs.cat_ranges([f"{TEST_BUCKET_NAME}/path"], [], [])
